f671f94145a22ecc9248b3df2b47340cab45f28b
Luis Vieira <luis.vieira@s2it.com.br>
2016-09-16 15:49:44 -0300

    change all files to same package. add env feature to get facility name

--- gelf_formatter.go
+++ gelf_formatter.go
@@ -1,37 +1,42 @@
 package xild
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-    "os"
-    "time"
-    "bytes"
 	"github.com/Sirupsen/logrus"
+	"github.com/caarlos0/env"
+
+	"os"
+	"time"
 )
 
 type GELFFormatter struct {
 }
- 
+
 type Message struct {
-	Version  string                 `json:"version"`
-	Host     string                 `json:"host"`
-	Short    string                 `json:"short_message"`
-	Full     string                 `json:"full_message"`
-	TimeUnix float64                `json:"timestamp"`
-	Level    int32                  `json:"level"`
-	Facility string                 `json:"facility"`
-	File     string                 `json:"file"`
-	Line     int                    `json:"line"`
-	Extra    map[string]interface{} `json:"-"`
+	Version   string                 `json:"version"`
+	Host      string                 `json:"host"`
+	Short     string                 `json:"short_message"`
+	Full      string                 `json:"full_message"`
+	Timestamp float64                `json:"timestamp"`
+	Level     int32                  `json:"level"`
+	File      string                 `json:"file"`
+	Line      int                    `json:"line"`
+	Extra     map[string]interface{} `json:"-"`
 }
-  
+
+type ExtraEnvField struct {
+	Facility string `env:"APPLICATION_NAME"`
+}
+
 func (f *GELFFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	host, err := os.Hostname()
 	if err != nil {
 		host = "localhost"
 	}
 
-    // remove trailing and leading whitespace
+	// remove trailing and leading whitespace
 	p := bytes.TrimSpace([]byte(entry.Message))
 
 	// If there are newlines in the message, use the first line
@@ -44,28 +49,28 @@ func (f *GELFFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 		short = p[:i]
 		full = p
 	}
-    
-    level := int32(entry.Level) + 2 // logrus levels are lower than syslog by 2
 
-    
+	level := int32(entry.Level) + 2 // logrus levels are lower than syslog by 2
+
 	extra := map[string]interface{}{}
+	//set facility as application name or null
+	extra["_facility"] = ExtraEnvField.Facility
 	// Merge extra fields
 	for f, v := range entry.Data {
 		f = fmt.Sprintf("_%s", f) // "[...] every field you send and prefix with a _ (underscore) will be treated as an additional field."
 		extra[f] = v
 	}
 
-
-    m := Message{
-		Version:  "1.1",
-		Host:     host,
-		Short:    string(short),
-		Full:     string(full),
-		TimeUnix: float64(time.Now().UnixNano()/1000000) / 1000.,
-		Level:    level,
+	m := Message{
+		Version:   "1.1",
+		Host:      host,
+		Short:     string(short),
+		Full:      string(full),
+		TimeStamp: entry.Time.Format(time.DefaultTimestampFormat),
+		Level:     level,
 		// File:     entry.file, deprecated
 		// Line:     entry.line, deprecated
-		Extra:    extra,
+		Extra: extra,
 	}
 
 	serialized, err := json.Marshal(m)
