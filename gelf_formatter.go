11b95d03efb197959b28a24eaeb9444df6e09a3c
Luis Vieira <luis.vieira@s2it.com.br>
2016-09-16 18:51:00 -0300

    test for gelf_formatter

--- gelf_formatter.go
+++ gelf_formatter.go
@@ -47,17 +47,6 @@ func (f *GELFFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 
 	level := int32(entry.Level) + 2 // logrus levels are lower than syslog by 2
 
-	extra := map[string]interface{}{}
-	//set facility as application name or null
-	if appName := os.Getenv("APPLICATION_NAME"); appName != "" {
-		extra["_facility"] = appName
-	}
-	// Merge extra fields
-	for f, v := range entry.Data {
-		f = fmt.Sprintf("_%s", f) // "[...] every field you send and prefix with a _ (underscore) will be treated as an additional field."
-		extra[f] = v
-	}
-
 	m := Message{
 		Version:   "1.1",
 		Host:      host,
@@ -67,12 +56,35 @@ func (f *GELFFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 		Level:     level,
 		// File:     entry.file, deprecated
 		// Line:     entry.line, deprecated
-		Extra: extra,
 	}
 
 	serialized, err := json.Marshal(m)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to marshal fields to JSON, %v", err)
 	}
+
+	extra := map[string]interface{}{}
+	//set facility as application name or null
+	if appName := os.Getenv("APPLICATION_NAME"); appName != "" {
+		extra["_appName"] = appName
+	}
+	// Merge extra fields
+	for f, v := range entry.Data {
+		f = fmt.Sprintf("_%s", f) // "[...] every field you send and prefix with a _ (underscore) will be treated as an additional field."
+		extra[f] = v
+	}
+
+	if len(extra) > 0 {
+		extraSerialized, err := json.Marshal(extra)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to marshal fields to JSON, %v", err)
+		}
+		//remove last } from main json
+		serialized[len(serialized)-1] = ','
+		extraSerialized[0] = ' '
+		serialized = append(serialized, extraSerialized...)
+	}
+
 	return append(serialized, '\n'), nil
+
 }
--- gelf_formatter_test.go
+++ gelf_formatter_test.go
@@ -1,1 +1,101 @@
 package xild
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+)
+
+// GELF Format Specification
+// Version 1.1 (11/2013)
+
+// A GELF message is a GZIP’d or ZLIB’d JSON string with the following fields:
+
+// version string (UTF-8)
+// short_message string (UTF-8)
+// full_message string (UTF-8)
+// timestamp number
+// level number
+// _[additional field] string (UTF-8) or number
+//more information: http://docs.graylog.org/en/2.1/pages/gelf.html
+func TestLogFormart(t *testing.T) {
+	expectedFields := []string{"version", "short_message", "full_message", "timestamp", "level"}
+	formatter := &GELFFormatter{}
+
+	b, err := formatter.Format(logrus.WithField("body", "o body"))
+	if err != nil {
+		t.Fatal("Unable to format entry: ", err)
+	}
+
+	entry := make(map[string]interface{})
+	err = json.Unmarshal(b, &entry)
+	if err != nil {
+		t.Fatal("Unable to unmarshal formatted entry: ", err)
+	}
+
+	if ok, value := validExpectedField(expectedFields, entry); !ok {
+		t.Fatalf("[GreyLog required field not found %s the values founds are %s]", value, entry)
+	}
+}
+
+func TestWithLogAddFieldsAsAdditionalFields(t *testing.T) {
+	expectedFields := []string{"_body", "_err"}
+
+	formatter := &GELFFormatter{}
+
+	b, err := formatter.Format(logrus.WithFields(logrus.Fields{
+		"body": "value of body",
+		"err":  "value of err",
+	}))
+	if err != nil {
+		t.Fatal("Unable to format entry: ", err)
+	}
+
+	entry := make(map[string]interface{})
+	err = json.Unmarshal(b, &entry)
+	if err != nil {
+		t.Fatal("Unable to unmarshal formatted entry: ", err)
+	}
+
+	if ok, value := validExpectedField(expectedFields, entry); !ok {
+		t.Fatalf("[Additional field not found %s the values founds are %s]", value, entry)
+	}
+
+}
+
+func TestWithLogSetFacilityWithEnvSetted(t *testing.T) {
+	expectedFields := []string{"_body", "_err", "_appName"}
+	os.Setenv("APPLICATION_NAME", "TEST-XILD")
+
+	formatter := &GELFFormatter{}
+
+	b, err := formatter.Format(logrus.WithFields(logrus.Fields{
+		"body": "value of body",
+		"err":  "value of err",
+	}))
+	if err != nil {
+		t.Fatal("Unable to format entry: ", err)
+	}
+
+	entry := make(map[string]interface{})
+	err = json.Unmarshal(b, &entry)
+	if err != nil {
+		t.Fatal("Unable to unmarshal formatted entry: ", err)
+	}
+
+	if ok, value := validExpectedField(expectedFields, entry); !ok {
+		t.Fatalf("[Additional field not found %s the values founds are %s]", value, entry)
+	}
+
+}
+
+func validExpectedField(expectedFields []string, entry map[string]interface{}) (bool, string) {
+	for _, value := range expectedFields {
+		if _, ok := entry[value]; !ok {
+			return false, value
+		}
+	}
+	return true, ""
+}
